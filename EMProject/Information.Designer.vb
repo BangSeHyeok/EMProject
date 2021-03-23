516cb27a2ce5825d932fef881c97bfbe0546a5a7
LeeGwangHo <dlrhkdgh2279@gmail.com>
2021-03-23 16:14:18 +0900

    16:14

--- EMProject/Information.Designer.vb
+++ EMProject/Information.Designer.vb
@@ -22,17 +22,265 @@ Partial Class Information
     '코드 편집기에서는 수정하지 마세요.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lbl_qNumber = New System.Windows.Forms.Label()
+        Me.lbl_qname = New System.Windows.Forms.Label()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lbl_introduction = New System.Windows.Forms.Label()
+        Me.lbl_Name = New System.Windows.Forms.Label()
+        Me.lbl_Number = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lbl_Position = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'lbl_qNumber
+        '
+        Me.lbl_qNumber.AutoSize = True
+        Me.lbl_qNumber.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_qNumber.Location = New System.Drawing.Point(47, 304)
+        Me.lbl_qNumber.Name = "lbl_qNumber"
+        Me.lbl_qNumber.Size = New System.Drawing.Size(85, 25)
+        Me.lbl_qNumber.TabIndex = 7
+        Me.lbl_qNumber.Text = "社員番号"
+        '
+        'lbl_qname
+        '
+        Me.lbl_qname.AutoSize = True
+        Me.lbl_qname.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_qname.Location = New System.Drawing.Point(47, 233)
+        Me.lbl_qname.Name = "lbl_qname"
+        Me.lbl_qname.Size = New System.Drawing.Size(48, 25)
+        Me.lbl_qname.TabIndex = 6
+        Me.lbl_qname.Text = "名前"
+        '
+        'btn_update
+        '
+        Me.btn_update.Font = New System.Drawing.Font("맑은 고딕", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_update.Location = New System.Drawing.Point(47, 527)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(87, 36)
+        Me.btn_update.TabIndex = 5
+        Me.btn_update.Text = "編集"
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.EMProject.My.Resources.Resources.다운로드
+        Me.PictureBox1.Location = New System.Drawing.Point(47, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(173, 201)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'lbl_introduction
+        '
+        Me.lbl_introduction.AutoSize = True
+        Me.lbl_introduction.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_introduction.Location = New System.Drawing.Point(47, 385)
+        Me.lbl_introduction.Name = "lbl_introduction"
+        Me.lbl_introduction.Size = New System.Drawing.Size(84, 25)
+        Me.lbl_introduction.TabIndex = 8
+        Me.lbl_introduction.Text = "自己紹介"
+        '
+        'lbl_Name
+        '
+        Me.lbl_Name.AutoSize = True
+        Me.lbl_Name.Location = New System.Drawing.Point(47, 267)
+        Me.lbl_Name.Name = "lbl_Name"
+        Me.lbl_Name.Size = New System.Drawing.Size(55, 15)
+        Me.lbl_Name.TabIndex = 10
+        Me.lbl_Name.Text = "イカンホ"
+        '
+        'lbl_Number
+        '
+        Me.lbl_Number.AutoSize = True
+        Me.lbl_Number.Location = New System.Drawing.Point(47, 343)
+        Me.lbl_Number.Name = "lbl_Number"
+        Me.lbl_Number.Size = New System.Drawing.Size(63, 15)
+        Me.lbl_Number.TabIndex = 11
+        Me.lbl_Number.Text = "75131006"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(47, 425)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(433, 96)
+        Me.RichTextBox1.TabIndex = 12
+        Me.RichTextBox1.Text = ""
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(512, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(280, 192)
+        Me.Panel1.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(120, 25)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "現在勤務常態"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 15)
+        Me.Label1.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(382, 233)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 25)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "現場名前"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(382, 304)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 25)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "現場場所"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(238, 343)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 15)
+        Me.Label6.TabIndex = 17
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.Location = New System.Drawing.Point(203, 233)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 25)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "役割"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(203, 267)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(67, 15)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "エンジニア"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(382, 335)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(84, 23)
+        Me.TextBox1.TabIndex = 20
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(382, 267)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(84, 23)
+        Me.TextBox2.TabIndex = 21
+        '
+        'lbl_Position
+        '
+        Me.lbl_Position.AutoSize = True
+        Me.lbl_Position.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_Position.Location = New System.Drawing.Point(203, 304)
+        Me.lbl_Position.Name = "lbl_Position"
+        Me.lbl_Position.Size = New System.Drawing.Size(48, 25)
+        Me.lbl_Position.TabIndex = 22
+        Me.lbl_Position.Text = "職級"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(203, 335)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(84, 23)
+        Me.TextBox3.TabIndex = 23
         '
         'Information
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1264, 587)
+        Me.ClientSize = New System.Drawing.Size(1290, 627)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.lbl_Position)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.lbl_Number)
+        Me.Controls.Add(Me.lbl_Name)
+        Me.Controls.Add(Me.lbl_introduction)
+        Me.Controls.Add(Me.lbl_qNumber)
+        Me.Controls.Add(Me.lbl_qname)
+        Me.Controls.Add(Me.btn_update)
+        Me.Controls.Add(Me.PictureBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Information"
         Me.Text = "Information"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents lbl_qNumber As Label
+    Friend WithEvents lbl_qname As Label
+    Friend WithEvents btn_update As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lbl_introduction As Label
+    Friend WithEvents lbl_Name As Label
+    Friend WithEvents lbl_Number As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents lbl_Position As Label
+    Friend WithEvents TextBox3 As TextBox
 End Class
--- EMProject/My Project/Resources.Designer.vb
+++ EMProject/My Project/Resources.Designer.vb
@@ -69,5 +69,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        '''</summary>
+        Friend ReadOnly Property 다운로드() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("다운로드", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
