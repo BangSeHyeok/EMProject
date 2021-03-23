9215a133c0b96f4ed9aaf1bf7b7046c9b21d9b25
Bangsehyeok <jhb923@naver.com>
2021-03-23 15:10:15 +0900

    Real Start

--- EMProject/Form1.vb
+++ EMProject/Form1.vb
@@ -1,9 +1,67 @@
 ﻿Public Class Form1
+    Dim home As Home
+    Dim calendar As Calendar
+    Dim information As Information
+    Dim notice As Notice
+    Dim old_name As String
     Private Sub Form1_Load(sender As Object, e As EventArgs)
-
+        home = New Home()
+        home.TopLevel = False  'toplevel 을 false 로 변경하면 panel form 에 docking 가능
+        home.Show()
+        Me.Panel1.Controls.Add(home)
+        old_name = "Main"
+    End Sub
+    Private Sub lbl_Main_Click(sender As Object, e As EventArgs) Handles lbl_Main.Click
+        old_name = "Main"
+        Panel_Find("Main")
+    End Sub
+    Private Sub Panel_Find(name As String)
+        For Each p As Control In Panel1.Controls
+            If old_name = p.Name Then
+                Return
+            End If
+            Panel1.Controls.Remove(p)
+            p.Dispose()
+        Next
+        If name = "Main" Then
+            MessageBox.Show("main")
+            home = New Home()
+            home.TopLevel = False
+            home.Show()
+            Me.Panel1.Controls.Add(home)
+        ElseIf name = "Calendar" Then
+            MessageBox.Show("calendar")
+            calendar = New Calendar
+            calendar.TopLevel = False
+            calendar.Show()
+            Me.Panel1.Controls.Add(calendar)
+        ElseIf name = "Information" Then
+            MessageBox.Show("information")
+            information = New Information
+            information.TopLevel = False
+            information.Show()
+            Me.Panel1.Controls.Add(information)
+        ElseIf name = "Notice" Then
+            MessageBox.Show("notice")
+            notice = New Notice
+            notice.TopLevel = False
+            notice.Show()
+            Me.Panel1.Controls.Add(notice)
+        End If
     End Sub
 
-    Private Sub Label3_Click(sender As Object, e As EventArgs)
+    Private Sub lbl_Calendar_Click(sender As Object, e As EventArgs) Handles lbl_Calendar.Click
+        old_name = "Calendar"
+        Panel_Find("Calendar")
+    End Sub
 
+    Private Sub lbl_Information_Click(sender As Object, e As EventArgs) Handles lbl_Information.Click
+        old_name = "Information"
+        Panel_Find("Information")
+    End Sub
+
+    Private Sub lbl_Notice_Click(sender As Object, e As EventArgs) Handles lbl_Notice.Click
+        old_name = "Notice"
+        Panel_Find("Notice")
     End Sub
 End Class
