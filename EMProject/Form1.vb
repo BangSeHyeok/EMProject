bfb4076d115e829f09d56da8d15affec0009a081
Bangsehyeok <jhb923@naver.com>
2021-03-23 14:49:35 +0900

    Title

--- EMProject/Form1.vb
+++ EMProject/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
