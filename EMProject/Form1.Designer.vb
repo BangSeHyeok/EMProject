6a88baf15813d23f2c6ef9a6a25c2218d96991a4
LeeGwangHo <dlrhkdgh2279@gmail.com>
2021-03-23 14:12:58 +0900

    FIrst Start

--- EMProject/Form1.Designer.vb
+++ EMProject/Form1.Designer.vb
@@ -37,7 +37,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(570, 231)
+        Me.Button2.Location = New System.Drawing.Point(533, 110)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 1
