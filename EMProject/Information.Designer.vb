683080edb63a866c69aeffbadfdd3788fa544efd
LeeGwangHo <dlrhkdgh2279@gmail.com>
2021-03-25 13:47:59 +0900

    0325.

--- EMProject/Information.Designer.vb
+++ EMProject/Information.Designer.vb
@@ -30,9 +30,6 @@ Partial Class Information
         Me.lbl_Name = New System.Windows.Forms.Label()
         Me.lbl_Number = New System.Windows.Forms.Label()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
@@ -43,15 +40,34 @@ Partial Class Information
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.lbl_Position = New System.Windows.Forms.Label()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lbl_qNumber
         '
         Me.lbl_qNumber.AutoSize = True
         Me.lbl_qNumber.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_qNumber.Location = New System.Drawing.Point(47, 304)
+        Me.lbl_qNumber.Location = New System.Drawing.Point(244, 156)
         Me.lbl_qNumber.Name = "lbl_qNumber"
         Me.lbl_qNumber.Size = New System.Drawing.Size(85, 25)
         Me.lbl_qNumber.TabIndex = 7
@@ -61,7 +77,7 @@ Partial Class Information
         '
         Me.lbl_qname.AutoSize = True
         Me.lbl_qname.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_qname.Location = New System.Drawing.Point(47, 233)
+        Me.lbl_qname.Location = New System.Drawing.Point(244, 90)
         Me.lbl_qname.Name = "lbl_qname"
         Me.lbl_qname.Size = New System.Drawing.Size(48, 25)
         Me.lbl_qname.TabIndex = 6
@@ -70,7 +86,7 @@ Partial Class Information
         'btn_update
         '
         Me.btn_update.Font = New System.Drawing.Font("맑은 고딕", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btn_update.Location = New System.Drawing.Point(47, 527)
+        Me.btn_update.Location = New System.Drawing.Point(354, 487)
         Me.btn_update.Name = "btn_update"
         Me.btn_update.Size = New System.Drawing.Size(87, 36)
         Me.btn_update.TabIndex = 5
@@ -79,8 +95,7 @@ Partial Class Information
         '
         'PictureBox1
         '
-        Me.PictureBox1.Image = Global.EMProject.My.Resources.Resources.다운로드
-        Me.PictureBox1.Location = New System.Drawing.Point(47, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(47, 90)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(173, 201)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -91,7 +106,7 @@ Partial Class Information
         '
         Me.lbl_introduction.AutoSize = True
         Me.lbl_introduction.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_introduction.Location = New System.Drawing.Point(47, 385)
+        Me.lbl_introduction.Location = New System.Drawing.Point(47, 334)
         Me.lbl_introduction.Name = "lbl_introduction"
         Me.lbl_introduction.Size = New System.Drawing.Size(84, 25)
         Me.lbl_introduction.TabIndex = 8
@@ -100,7 +115,7 @@ Partial Class Information
         'lbl_Name
         '
         Me.lbl_Name.AutoSize = True
-        Me.lbl_Name.Location = New System.Drawing.Point(47, 267)
+        Me.lbl_Name.Location = New System.Drawing.Point(244, 124)
         Me.lbl_Name.Name = "lbl_Name"
         Me.lbl_Name.Size = New System.Drawing.Size(55, 15)
         Me.lbl_Name.TabIndex = 10
@@ -109,7 +124,7 @@ Partial Class Information
         'lbl_Number
         '
         Me.lbl_Number.AutoSize = True
-        Me.lbl_Number.Location = New System.Drawing.Point(47, 343)
+        Me.lbl_Number.Location = New System.Drawing.Point(244, 195)
         Me.lbl_Number.Name = "lbl_Number"
         Me.lbl_Number.Size = New System.Drawing.Size(63, 15)
         Me.lbl_Number.TabIndex = 11
@@ -117,44 +132,17 @@ Partial Class Information
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(47, 425)
+        Me.RichTextBox1.Location = New System.Drawing.Point(47, 374)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(433, 96)
+        Me.RichTextBox1.Size = New System.Drawing.Size(402, 96)
         Me.RichTextBox1.TabIndex = 12
         Me.RichTextBox1.Text = ""
-        '
-        'Panel1
-        '
-        Me.Panel1.Controls.Add(Me.Label2)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(512, 12)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(280, 192)
-        Me.Panel1.TabIndex = 13
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(12, 9)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(120, 25)
-        Me.Label2.TabIndex = 11
-        Me.Label2.Text = "現在勤務常態"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 68)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(0, 15)
-        Me.Label1.TabIndex = 10
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(382, 233)
+        Me.Label3.Location = New System.Drawing.Point(354, 155)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(84, 25)
         Me.Label3.TabIndex = 14
@@ -164,7 +152,7 @@ Partial Class Information
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(382, 304)
+        Me.Label4.Location = New System.Drawing.Point(354, 226)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(84, 25)
         Me.Label4.TabIndex = 15
@@ -173,7 +161,7 @@ Partial Class Information
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(238, 343)
+        Me.Label6.Location = New System.Drawing.Point(279, 266)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(0, 15)
         Me.Label6.TabIndex = 17
@@ -182,16 +170,16 @@ Partial Class Information
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label7.Location = New System.Drawing.Point(203, 233)
+        Me.Label7.Location = New System.Drawing.Point(244, 90)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(48, 25)
         Me.Label7.TabIndex = 18
-        Me.Label7.Text = "役割"
+        Me.Label7.Text = "名前"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(203, 267)
+        Me.Label8.Location = New System.Drawing.Point(354, 114)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(67, 15)
         Me.Label8.TabIndex = 19
@@ -199,14 +187,14 @@ Partial Class Information
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(382, 335)
+        Me.TextBox1.Location = New System.Drawing.Point(354, 257)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(84, 23)
         Me.TextBox1.TabIndex = 20
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(382, 267)
+        Me.TextBox2.Location = New System.Drawing.Point(354, 189)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(84, 23)
         Me.TextBox2.TabIndex = 21
@@ -215,7 +203,7 @@ Partial Class Information
         '
         Me.lbl_Position.AutoSize = True
         Me.lbl_Position.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_Position.Location = New System.Drawing.Point(203, 304)
+        Me.lbl_Position.Location = New System.Drawing.Point(244, 227)
         Me.lbl_Position.Name = "lbl_Position"
         Me.lbl_Position.Size = New System.Drawing.Size(48, 25)
         Me.lbl_Position.TabIndex = 22
@@ -223,16 +211,158 @@ Partial Class Information
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(203, 335)
+        Me.TextBox3.Location = New System.Drawing.Point(244, 258)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(84, 23)
         Me.TextBox3.TabIndex = 23
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.DataGridView1.Location = New System.Drawing.Point(463, 214)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(438, 163)
+        Me.DataGridView1.TabIndex = 24
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "날짜"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "출근시간"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "퇴근시간"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "근무시간"
+        Me.Column4.Name = "Column4"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(463, 160)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 23)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "←"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(512, 160)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(30, 23)
+        Me.Button3.TabIndex = 27
+        Me.Button3.Text = "→"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(463, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 15)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "月별 근무시간이력"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(463, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 15)
+        Me.Label2.TabIndex = 29
+        Me.Label2.Text = "月 근무상황"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(463, 86)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(152, 23)
+        Me.ProgressBar1.TabIndex = 30
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.DataGridView2.Location = New System.Drawing.Point(920, 68)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowTemplate.Height = 25
+        Me.DataGridView2.Size = New System.Drawing.Size(358, 490)
+        Me.DataGridView2.TabIndex = 31
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "이름"
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "사원번호"
+        Me.Column6.Name = "Column6"
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "직급"
+        Me.Column7.Name = "Column7"
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "카테고리"
+        Me.Column8.Name = "Column8"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(920, 35)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 15)
+        Me.Label5.TabIndex = 32
+        Me.Label5.Text = "회사원 리스트"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("맑은 고딕", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(354, 80)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 25)
+        Me.Label9.TabIndex = 33
+        Me.Label9.Text = "役割"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("맑은 고딕", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(47, 35)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(118, 28)
+        Me.Label10.TabIndex = 34
+        Me.Label10.Text = "information"
         '
         'Information
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1290, 627)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.lbl_Position)
         Me.Controls.Add(Me.TextBox2)
@@ -242,7 +372,6 @@ Partial Class Information
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.lbl_Number)
         Me.Controls.Add(Me.lbl_Name)
@@ -253,10 +382,10 @@ Partial Class Information
         Me.Controls.Add(Me.PictureBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Information"
-        Me.Text = "Information"
+        Me.Text = "sssssdsd"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -270,9 +399,6 @@ Partial Class Information
     Friend WithEvents lbl_Name As Label
     Friend WithEvents lbl_Number As Label
     Friend WithEvents RichTextBox1 As RichTextBox
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
@@ -283,4 +409,22 @@ Partial Class Information
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents lbl_Position As Label
     Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
 End Class
